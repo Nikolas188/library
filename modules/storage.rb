70374b4e443b42d243fbe26988ca635861d36804
Nikolas188 <mikolarib8888@gmail.com>
2022-07-20 21:31:38 +0300

    defload

--- modules/storage.rb
+++ modules/storage.rb
@@ -1,6 +1,11 @@
 module Storage
+
   def save_to_yaml(collection)
     file_name = "./date/#{collection.first.class.to_s.downcase}s.yml"
     File.open(file_name, "w") { |file| file.write(collection.to_yaml) }
+  end
+
+  def load(collection)
+    File.safe_load(File.read("./date/#{collection.yml}"), Psyh, [], [], try)
   end
 end
